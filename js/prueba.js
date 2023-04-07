967e0949dc63034e170545a341c806771c7570b4
juan carlos macias <juancmaciassalvador@gmail.com>
2023-04-07 08:34:35 +0200

    Correcciones

--- js/prueba.js
+++
@@ -1,7 +0,0 @@
-fetch('/js/texto.html')
-  .then(res => res.text())
-  .then(content => {
-    let lines = content.split(/\n/);
-    document.getElementById("pruecarga").innerHTML=lines;
-    lines.forEach(line => console.log(line));
-  });
